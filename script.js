ee3db18eda2c21df6eeac16a30793b1d1451899d
Mohit Bisht <mail.mohitbisht@gmail.com>
2023-12-03 23:23:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+function togglePrompt() {
+    const predefinedPromptSection = document.getElementById('predefinedPromptSection');
+    const customPromptSection = document.getElementById('customPromptSection');
+    const promptType = document.getElementById('promptType').value;
+
+    if (promptType === 'predefined') {
+        predefinedPromptSection.style.display = 'block';
+        customPromptSection.style.display = 'none';
+    } else {
+        predefinedPromptSection.style.display = 'none';
+        customPromptSection.style.display = 'block';
+    }
+}
+
 function generateContent() {
     const platform = document.getElementById('platform').value;
-    const predefinedPrompt = document.getElementById('predefinedPrompts').value;
+    const promptType = document.getElementById('promptType').value;
+    let prompt;
+
+    if (promptType === 'predefined') {
+        prompt = document.getElementById('predefinedPrompts').value;
+    } else {
+        prompt = document.getElementById('customPrompt').value;
+    
     const generatedContent = 'Your generated content will be here.';
 
     document.getElementById('output').innerHTML = generatedContent;
